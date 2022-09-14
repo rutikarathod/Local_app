9b9fe6627560a82147b28e0adad1de3a644ccc75
rutikarathod <ruturathod1308@gmail.com>
2022-09-14 11:25:12 +0550

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## local
+
+local
+
+#### License
+
+MIT
---
+++ local/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "local",
+			"type": "module",
+			"label": _("local")
+		}
+	]
