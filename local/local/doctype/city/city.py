f5e0842fe6fd49bfbd4be20fbe59ff5ba570ed6b
rutikarathod <ruturathod1308@gmail.com>
2022-09-14 11:48:54 +0530

    doctype folder add

---
+++ local/local/doctype/city/city.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, local and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class City(Document):
+	pass
---
+++ local/local/doctype/registration_form/registration_form.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, local and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('registration_form', {
+	// refresh: function(frm) {
+
+	// }
+});
